a867c063ad8d6be4a665c9df5280d6dedbf4b17c
Armel <armelfli50@gmail.com>
2020-02-20 15:48:29 -0600

    user can successfully edit a question, change its type and add more options

--- app/controllers/survey_questions_controller.rb
+++ app/controllers/survey_questions_controller.rb
@@ -1,11 +1,14 @@
 class SurveyQuestionsController < ApplicationController
   def index
-    @questions = SurveyQuestion.all
+    @survey = Survey.find( params.permit("survey_id")[:survey_id])
+    @questions = SurveyQuestion.all.where("survey_id= ?", @survey.id)
+
   end
 
   def new
     @survey = Survey.find( params.permit("survey_id")[:survey_id])
     @question = SurveyQuestion.new
+    
   end
 
   def create
@@ -27,10 +30,56 @@ class SurveyQuestionsController < ApplicationController
   end
 
   def edit
-    @question = SurveyQuestion.find(params[:id])
+    @survey = Survey.find(params.require(:survey_id))
+    @question = SurveyQuestion.find(params.require("id"))
   end
 
   def update
+    #find the survey and the question
+    @survey = Survey.find(params.require(:survey_id))
+    @question = SurveyQuestion.find(params.require("id"))
+
+   if survey_question_params[:questions1]
+
+      @question.is_fixed = true 
+      @question.question = survey_question_params[:questions1][:question]
+      #remove existing option
+      if @question.question_options.length > 0 
+        @question.question_options.each {|option| option.delete}
+        
+      end
+      @question.save
+
+      #iterate through the options
+      survey_question_params[:questions1][:options].each do |key,val|
+        # binding.pry
+        if key != "new"
+          option = QuestionOption.find(key)
+          option.option_text = survey_question_params[:questions1][:options][key]
+          # binding.pry
+          option.save 
+        else
+          survey_question_params[:questions1][:options][key].each do |option|
+            new_option = @question.question_options.create({option_text: option})
+          end
+          # binding.pry
+        end
+      
+
+      end
+   else
+     @question.is_fixed = false 
+      @question.question = survey_question_params[:question]
+      @question.save
+   end
+  #check if there is an error
+   if !@survey.errors.any? 
+    redirect_to @survey
+    
+  else 
+    redirect_to :back
+  end
+    
   end
 
   private
---
+++ app/javascript/packs/edit_single_question.js
@@ -0,0 +1,111 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //select the buttons defined in edit survey_questions
+    const toggle_button = document.querySelector("button.toggle_free_fixed")
+    const div_edit_question = document.querySelector("div.div_edit_question")
+    const question =div_edit_question.getAttribute("question")
+    const options =  JSON.parse( div_edit_question.getAttribute("options") )
+    console.log( div_edit_question.getAttribute("options") )
+    const buttonMoreExist = document.querySelector("button.moreOptions") 
+
+    //here we check if the button add more already exist, if so we want to give the option to the user to add more options
+    if(buttonMoreExist){
+        
+        addMore(buttonMoreExist)
+    }
+    
+    //add event listerner 
+    toggle_button.addEventListener("click", e => {
+        e.preventDefault()
+        if(toggle_button.innerHTML.includes("free")){
+            console.log(toggle_button.innerHTML)
+            toggle_button.innerHTML = "Expect fixed response?"
+            div_edit_question.innerHTML = `
+              <label>Question for free response</label>
+              <textarea name="survey_question[question]" placeholder="Enter your question here" >${question}</textarea>
+            
+            `
+            //hide the add more button if the user is trying to create a question for a free response
+            document.querySelector("button.moreOptions").style.display = "none"
+
+           if( document.querySelector("div.optionContainer") ){
+            document.querySelector("div.optionContainer").innerHTML = ""
+           }
+
+        }else{
+            if(document.querySelector("button.moreOptions")){
+                document.querySelector("button.moreOptions").style.display = "block"
+
+            }
+            toggle_button.innerHTML = "Expect free response?"
+            div_edit_question.innerHTML = `
+                    <label class="labelForFixed">Question for fixed response</label>
+                    <textarea placeholder="Enter your question here" name="survey_question[questions1][question]" >${question}</textarea> 
+                      
+                    `
+            if(options && options.length > 0){
+              
+                    options.forEach(e =>{
+                        div_edit_question.innerHTML += `
+                                <input name='survey_question[questions1][options][${e.id}]' value=${e.option_text} >
+                        `
+                    })
+            }else{
+                div_edit_question.innerHTML += `
+                    <input name='survey_question[questions1][options][new][]' placeholder="Enter a possible answer" >
+                `
+                
+            }
+           
+            //create a button More option if it did not exist
+            if(!buttonMoreExist && !document.querySelector("button.moreOptions")){
+                const newBtn = document.createElement("button")
+                newBtn.innerHTML = "More options"
+                newBtn.classList.add("moreOptions")
+                document.querySelector("div.buttonHolder").appendChild(newBtn)
+                
+                addMore(newBtn)
+            }
+          
+        }
+       
+    })
+
+
+//addMore function
+    function addMore(moreButton){
+      //add event to the button
+      moreButton.addEventListener("click",e =>{
+          e.preventDefault()
+    
+          //create a new div to host the new input
+          const newDiv = document.createElement("div")
+          newDiv.classList.add("newDiv")
+    
+          //create new input for options
+          new_input = document.createElement("input")
+          new_input.name='survey_question[questions1][options][new][]' 
+          new_input.placeholder = "Enter a possible answer" 
+    
+          //create a span for deletion of added fields
+          const span_2X = document.createElement("span")
+          span_2X.innerHTML = "X"
+          span_2X.addEventListener("click", () => {
+            span_2X.parentNode.remove()
+          })
+      
+          //put everything together
+          newDiv.appendChild(new_input)
+          newDiv.appendChild(span_2X)
+          document.querySelector("div.optionContainer").appendChild(newDiv)
+    
+        
+      })
+    }
+
+ 
+
+})
+
+
+
+  
